cbbfe346f8c494f00cab232a1642471a5fcdfc38
Eric Zhou <yiruiz2@illinois.edu>
2024-10-21 11:21:13 -0500

    add flush at end of stdout

--- src/printUtils.h
+++ src/printUtils.h
@@ -2,19 +2,29 @@
 #define __PRINT_UTILS_H__
 
 // debug
-#define dprintf(format, ...) \
-    printf(("\033[96m" + std::string(format) + "\033[0m").c_str(), __VA_ARGS__)
+#define dprintf(format, ...) do {                                                       \
+        printf(("\033[96m" + std::string(format) + "\033[0m").c_str(), __VA_ARGS__);    \
+        fflush(stdout);                                                                 \
+    } while (false)
 // info
-#define iprintf(format, ...) \
-    printf(("\033[92m" + std::string(format) + "\033[0m").c_str(), __VA_ARGS__)
+#define iprintf(format, ...) do {                                                       \
+        printf(("\033[92m" + std::string(format) + "\033[0m").c_str(), __VA_ARGS__);    \
+        fflush(stdout);                                                                 \
+    } while (false)
 // notice
-#define nprintf(format, ...) \
-    printf(("\033[94m" + std::string(format) + "\033[0m").c_str(), __VA_ARGS__)
+#define nprintf(format, ...) do {                                                       \
+        printf(("\033[94m" + std::string(format) + "\033[0m").c_str(), __VA_ARGS__);    \
+        fflush(stdout);                                                                 \
+    } while (false)
 // warning
-#define wprintf(format, ...) \
-    printf(("\033[93m" + std::string(format) + "\033[0m").c_str(), __VA_ARGS__)
+#define wprintf(format, ...) do {                                                       \
+        printf(("\033[93m" + std::string(format) + "\033[0m").c_str(), __VA_ARGS__);    \
+        fflush(stdout);                                                                 \
+    } while (false)
 // error
-#define eprintf(format, ...) \
-    printf(("\033[91m" + std::string(format) + "\033[0m").c_str(), __VA_ARGS__)
+#define eprintf(format, ...) do {                                                       \
+        printf(("\033[91m" + std::string(format) + "\033[0m").c_str(), __VA_ARGS__);    \
+        fflush(stdout);                                                                 \
+    } while (false)
 
 #endif
